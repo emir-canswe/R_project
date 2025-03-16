0fdc6d618ac82c8cf6127aeda3cd3a504876958e
emir-canswe <canemircan38@gmail.com>
2025-03-16 17:34:54 +0300

    Create r_atamaalr.R

---
+++ r_atamaalr.R
@@ -0,0 +1,25 @@
+print("ATAMALAR")
+# <-
+# =
+#bir degişkeni bie sayiya eşitlemek için yukardaki simgeler kullanilir
+
+#  atama kisimi için <- işareti kullanilir
+a<-2
+b<-34
+
+a+b#bu sekikde yaptiginda direct olaarak ciikti seklinde 36 cevaabi cikacaktir
+
+
+c<-a+b
+  
+#diger operatorler ee digerleri işle hemen hemen ayni farkli bir durum soz konusu degil
+
+veri<-1:20
+
+sqrt(c)#bir sayiniin karekokunu gosterir
+abs(-23)#mutlak deger atamasi yapar
+
+sessionInfo()
+getwd()
+
+ls()#degişkenleri gostermeye yarar(ls adinda bir fonksion)
