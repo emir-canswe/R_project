8a3595ce373c47a6832f33b44fd3e42403f37692
emir-canswe <canemircan38@gmail.com>
2025-03-16 17:30:28 +0300

    Create factor_konu_anlatimi.R

---
+++ factor_konu_anlatimi.R
@@ -0,0 +1,7 @@
+#-------factor konu anlatimi
+
+
+egitim <- factor(c("Lise", "Üniversite", "Yüksek Lisans", "Doktora"),
+                 levels = c("Lise", "Üniversite", "Yüksek Lisans", "Doktora"),
+                 ordered = TRUE)
+#faktor senin veri gorsellestimede kategorize etmen için iyi bir arac
